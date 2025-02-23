04303a36b35cb2a4540d94242fcf6dad8430bb67
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-02-23 23:05:25 +0700

    blade component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Contact</title>
-</head>
-
-<body>
-    <h1>Halaman Contact</h1>
-    <ul>
-        <li>
-            <a href="https://instagram.com/ekoapriliyani11">Instagram</a>
-        </li>
-        <li>
-            <a href="">LinkedIn</a>
-        </li>
-        <li>
-            <a href="">X</a>
-        </li>
-        <li>
-            <a href="">YouTube</a>
-        </li>
-    </ul>
-
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3 class="text-xl">Welcome to my contact</h3>
+</x-layout>
