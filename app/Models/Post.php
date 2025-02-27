fc31b854b1459c8f80f6485d44558e8d3669498b
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-02-27 09:18:31 +0700

    model factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,42 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Eko Apriliyani',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ipsam, labore
-architecto ut explicabo
-ipsum, illo omnis dolorem ullam suscipit velit fugit unde recusandae enim assumenda vitae, placeat veniam
-animi.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Eko Apriliyani',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ab corporis repellat
-rerum incidunt nemo fuga, quis, veritatis excepturi ea facilis consectetur. Magnam ea exercitationem modi
-doloribus hic eius sunt!'
-            ]
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-        if (!$post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
